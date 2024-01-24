fd0118b28724b286911a9781cc11c213af87be9c
Oleg Kostin <kov@lins.ru>
2024-01-24 14:26:43 +0300

    Added thunkFunction

--- frontend/src/components/BookForm/BookForm.jsx
+++ frontend/src/components/BookForm/BookForm.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { useDispatch } from 'react-redux'
-import axios from 'axios'
+import { thunkFunction } from '../../redux/slices/booksSlice'
 import { addBook } from '../../redux/slices/booksSlice'
 import createBookWithID from '../../utils/createBookWithID'
 import booksData from '../../data/books.json'
@@ -27,22 +27,8 @@ const BookForm = () => {
         dispatch(addBook(createBookWithID(booksData[randomIndex], 'random')))
     }
 
-    const handleAddRandomBookViaAPI = async () => {
-        try {
-            const res = await axios.get('http://localhost:4000/random-book')
-            if (res?.data?.title && res?.data?.author) {
-                dispatch(addBook(createBookWithID(res.data, 'API')))
-            }
-        } catch (error) {
-            console.log(`Error fetching random book ${error}`)
-        }
-
-        // const json = await fetch('http://localhost:5000/random-book')
-        //     .then((res) => res.json())
-        //     .catch((error) => console.log(`Произошла ошибка: ${error}`))
-        // if (json?.title && json?.author) {
-        //     dispatch(addBook(createBookWithID(json)))
-        // }
+    const handleAddRandomBookViaAPI = () => {
+        dispatch(thunkFunction)
     }
 
     return (
